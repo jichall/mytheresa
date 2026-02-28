97d7b8ef8db43472ff8e15ba4af70dc6b4d8881e
Rafael Campos Nunes <rcamposnunes@outlook.com>
2026-02-28 14:44:28 -0300

    Add migration of categories in a product
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

---
+++ sql/005-add-product-categories.sql
@@ -0,0 +1,10 @@
+ALTER TABLE products ADD COLUMN category_id INTEGER REFERENCES product_categories(id);
+
+INSERT INTO product_categories (code, name) VALUES
+('clothing', 'Clothing'),
+('shoes', 'Shoes'),
+('accessories', 'Accessories');
+
+UPDATE products SET category_id = (SELECT id FROM product_categories WHERE code = 'clothing') WHERE code IN ('PROD001', 'PROD004', 'PROD007');
+UPDATE products SET category_id = (SELECT id FROM product_categories WHERE code = 'shoes') WHERE code IN ('PROD002', 'PROD006');
+UPDATE products SET category_id = (SELECT id FROM product_categories WHERE code = 'accessories') WHERE code IN ('PROD003', 'PROD005', 'PROD008');
