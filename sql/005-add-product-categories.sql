596805c7dc9ac484088824c3c5335799f4aed178
Rafael Campos Nunes <rcamposnunes@outlook.com>
2026-02-28 14:50:25 -0300

    Add a view for the products
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- sql/005-add-product-categories.sql
+++ sql/005-add-product-categories.sql
@@ -8,3 +8,5 @@ INSERT INTO product_categories (code, name) VALUES
 UPDATE products SET category_id = (SELECT id FROM product_categories WHERE code = 'clothing') WHERE code IN ('PROD001', 'PROD004', 'PROD007');
 UPDATE products SET category_id = (SELECT id FROM product_categories WHERE code = 'shoes') WHERE code IN ('PROD002', 'PROD006');
 UPDATE products SET category_id = (SELECT id FROM product_categories WHERE code = 'accessories') WHERE code IN ('PROD003', 'PROD005', 'PROD008');
+
+CREATE VIEW vw_products AS SELECT p.code, pc.name, p.price FROM products p INNER JOIN product_categories pc ON p.category_id = pc.id;
